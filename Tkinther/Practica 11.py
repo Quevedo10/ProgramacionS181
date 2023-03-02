8eba00fcf24f2db4c9d6e0613cbfff8f3adeb714
Miguel Quevedo <miguelquevedo53@gmail.com>
2023-03-01 18:03:30 -0600

    Funciones para botones

--- Tkinther/Practica 11.py
+++ Tkinther/Practica 11.py
@@ -1,5 +1,21 @@
 
-from tkinter import Tk, Frame, Button
+from tkinter import Tk, Frame, Button, messagebox
+
+def mostrarMensaje():
+    messagebox.showinfo("Aviso", "Este mensaje es para avisar algo")
+    messagebox.showerror("Es un mensaje de error: ","Todo falló con éxito")
+    print (messagebox.askokcancel("Pregunta: ","¿El jugó con tu corazón?"))
+    #print (messagebox.askquestion(""))
+    #print (messagebox.askretrycancel(""))
+    #print (messagebox.askyesno(""))
+    #print (messagebox.askyesnocancel(""))
+
+#Función para agrefar botones
+
+def agregarBoton():
+    botonblanco.config(text = "+",bg="orange",fg="black")
+    botonNuevo = Button(seccion3, text = "Botón Nuevo")
+    botonNuevo.pack()
 
 #Creación ventana
 
@@ -18,7 +34,7 @@ seccion3 = Frame(ventana, bg = "#000000")
 seccion3.pack(expand = True, fill = 'both') 
 
 #Botones
-botonAzul = Button(seccion1, text = "Botón Azul", bg = "#f2f2f2")
+botonAzul = Button(seccion1, text = "Botón Azul", bg = "#f2f2f2", command = mostrarMensaje)
 botonAzul.place(x=60, y=60)
 
 botonAmarillo = Button(seccion2, text = "Botón Amarillo", bg = "#f2f2f2")
@@ -27,7 +43,7 @@ botonAmarillo.grid(row=0,column=0)
 botonNegro = Button(seccion2, text = "Botón Negro", bg = "#f2f2f2")
 botonNegro.grid(row=0,column=1)
 
-botonblanco = Button(seccion3, text = "Botón blanco", bg = "#f2f2f2")
+botonblanco = Button(seccion3, text = "Botón blanco", bg = "#f2f2f2", command = agregarBoton)
 botonblanco.pack()
 
 #Main de ejecución de ventana
