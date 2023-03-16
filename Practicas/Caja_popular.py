8fc8927187fe6e9b7c86818f39eea7e3a16f0595
Miguel Quevedo <miguelquevedo53@gmail.com>
2023-03-15 18:06:44 -0600

    primer commit examen

---
+++ Practicas/Caja_popular.py
@@ -0,0 +1,92 @@
+
+import tkinter as tk
+from logica import *
+
+class Cuenta:
+    def __init__(self, num_cuenta, titular, edad, saldo):
+        self.num_cuenta = num_cuenta
+        self.titular = titular
+        self.edad = edad
+        self.saldo = saldo
+
+    def consultar_saldo(self):
+        return self.saldo
+
+    def ingresar_efectivo(self, cantidad):
+        self.saldo += cantidad
+
+    def retirar_efectivo(self, cantidad):
+        if cantidad > self.saldo:
+            print("No tienes suficiente saldo para retirar esa cantidad.")
+        else:
+            self.saldo -= cantidad
+
+    def depositar_otra_cuenta(self, otra_cuenta, cantidad):
+        if cantidad > self.saldo:
+            print("No tienes suficiente saldo para realizar esa transferencia.")
+        else:
+            self.saldo -= cantidad
+            otra_cuenta.ingresar_efectivo(cantidad)
+
+class VentanaCuenta():
+    def __init__(self, master, cuenta):
+        self.cuenta = cuenta
+
+        self.label_num_cuenta = tk.Label(master, text="No. Cuenta: " + cuenta.num_cuenta)
+        self.label_titular = tk.Label(master, text="Titular: " + cuenta.titular)
+        self.label_edad = tk.Label(master, text="Edad: " + str(cuenta.edad))
+        self.label_saldo = tk.Label(master, text="Saldo: " + str(cuenta.saldo))
+
+        self.button_consultar_saldo = tk.Button(master, text="Consultar saldo", command=self.consultar_saldo)
+        self.button_ingresar_efectivo = tk.Button(master, text="Ingresar efectivo", command=self.ingresar_efectivo)
+        self.button_retirar_efectivo = tk.Button(master, text="Retirar efectivo", command=self.retirar_efectivo)
+        self.button_depositar_otra_cuenta = tk.Button(master, text="Depositar a otra cuenta", command=self.depositar_otra_cuenta)
+
+        self.label_num_cuenta.pack()
+        self.label_titular.pack()
+        self.label_edad.pack()
+        self.label_saldo.pack()
+        self.button_consultar_saldo.pack()
+        self.button_ingresar_efectivo.pack()
+        self.button_retirar_efectivo.pack()
+        self.button_depositar_otra_cuenta.pack()
+
+    def consultar_saldo(self):
+        saldo = self.cuenta.consultar_saldo()
+        tk.messagebox.showinfo("Saldo", "El saldo de la cuenta es: " + str(saldo))
+        self.label_saldo.config(text="Saldo: " + str(saldo))
+
+    def ingresar_efectivo(self):
+        cantidad = tk.simpledialog.askinteger("Ingresar efectivo", "Cantidad a ingresar:")
+        if cantidad is not None:
+            self.cuenta.ingresar_efectivo(cantidad)
+            tk.messagebox.showinfo("Ingresar efectivo", "Se ha ingresado " + str(cantidad) + " pesos.")
+            self.label_saldo.config(text="Saldo: " + str(self.cuenta.saldo))
+
+    def retirar_efectivo(self):
+        cantidad = tk.simpledialog.askinteger("Retirar efectivo", "Cantidad a retirar:")
+        if cantidad is not None:
+            self.cuenta.retirar_efectivo(cantidad)
+            tk.messagebox
+    def depositar_otra_cuenta(self):
+        otra_cuenta = tk.simpledialog.askstring("Depositar a otra cuenta", "No. de cuenta a depositar:")
+        cantidad = tk.simpledialog.askinteger("Depositar a otra cuenta", "Cantidad a depositar:")
+        if otra_cuenta is not None and cantidad is not None:
+            self.cuenta.depositar_otra_cuenta(otra_cuenta, cantidad)
+            tk.messagebox.showinfo("Depositar a otra cuenta", "Se ha depositado " + str(cantidad) + " pesos en la cuenta " + otra_cuenta + ".")
+            self.label_saldo.config(text="Saldo: " + str(self.cuenta.saldo))
+
+        if __name__ == "__main__":
+            cuentas = [
+            Cuenta("001", "Juan Perez", 25, 1000),
+            Cuenta("002", "Maria Gonzalez", 35, 500),
+            Cuenta("003", "Pedro Ramirez", 45, 2000),
+        ]
+
+    root = tk.Tk()
+    root.title("Caja Popular")
+
+    frame_cuentas = tk.Frame(root)
+    frame_cuentas.pack(side="left", padx=10, pady=10)
+
+    root.mainloop()
--- Practicas/generador.py
+++ Practicas/generador.py
@@ -1,14 +1,37 @@
-import string
-import random
-from tkinter import Button, Entry
+import datetime
+from tkinter import messagebox
+import tkinter as tk
 
-class generador():
-
-    def gen():
-        small_alphabets=string.ascii_lowercase
-        capital_alphabets=string.ascii_uppercase
-        numbers=string.digits
-        special_charecters=string.punctuation
-
-        all = small_alphabets+capital_alphabets+numbers+special_charecters
-        password_length=int(length_Box.get())   
+class App:
+    def __init__(self, master):
+        self.master = master
+        
+        # Crear los widgets de la interfaz
+        tk.Label(master, text="Nombre:").grid(row=0, column=0)
+        tk.Label(master, text="Apellido paterno:").grid(row=1, column=0)
+        tk.Label(master, text="Apellido materno:").grid(row=2, column=0)
+        tk.Label(master, text="Año de nacimiento:").grid(row=3, column=0)
+        tk.Label(master, text="Carrera:").grid(row=4, column=0)
+        
+        self.nombre_entry = tk.Entry(master)
+        self.apellido_paterno_entry = tk.Entry(master)
+        self.apellido_materno_entry = tk.Entry(master)
+        self.year_nacimiento_entry = tk.Entry(master)
+        self.carrera_entry = tk.Entry(master)
+        
+        self.nombre_entry.grid(row=0, column=1)
+        self.apellido_paterno_entry.grid(row=1, column=1)
+        self.apellido_materno_entry.grid(row=2, column=1)
+        self.year_nacimiento_entry.grid(row=3, column=1)
+        self.carrera_entry.grid(row=4, column=1)
+        
+        tk.Button(master, text="Generar matrícula", command=self.generar_matricula).grid(row=5, column=0, columnspan=2)
+    
+    def generar_matricula(self):
+        nombre = self.nombre_entry.get()
+        apellido_paterno = self.apellido_paterno_entry.get()
+        apellido_materno = self.apellido_materno_entry.get()
+        year_nacimiento = int(self.year_nacimiento_entry.get())
+        carrera = self.carrera_entry.get()
+        
+        matricula = generar_matricula(nombre, apellido_paterno, apellido_materno, year_nacimiento, carrera
