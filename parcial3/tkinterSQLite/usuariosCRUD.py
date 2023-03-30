6546f2b90632fab3484a3e3b0a81eb99ec80a249
Miguel Quevedo <miguelquevedo53@gmail.com>
2023-03-29 19:46:58 -0600

    practica 16

--- parcial3/tkinterSQLite/usuariosCRUD.py
+++ parcial3/tkinterSQLite/usuariosCRUD.py
@@ -18,7 +18,7 @@ def ejecutaSelectU():
     cadena = str(usu[0])+ " " + usu[1]+ " " + usu[2]+ " " + str(usu[3])
 
    if(usuario):
-       print(cadena)
+       textEnc.insert("0.0",cadena)
    else:
        messagebox.showinfo("Usuario no encontrado","Seguramente no exita en la base de datos")
    
@@ -63,8 +63,8 @@ txtid = Entry(pestaña2,textvariable=varBus).pack()
 btnBus = Button(pestaña2, text="Buscar",command=ejecutaSelectU).pack()
 
 subBus = Label(pestaña2,text="Encontrado: ", fg='blue', font=("Modern",15)).pack()
-textEnc = tk.Text(pestaña2,height=5,width=52).pack()
-
+textEnc = tk.Text(pestaña2,height=5,width=52)
+textEnc.pack()
 
 panel.add(pestaña1, text='Formulario de usuarios')
 panel.add(pestaña2, text='Buscar usuario')
