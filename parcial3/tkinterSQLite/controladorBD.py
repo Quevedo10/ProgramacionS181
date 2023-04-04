d5a121c672b8ac7ceaaf54f555141d6691127fbc
Miguel Quevedo <miguelquevedo53@gmail.com>
2023-04-03 20:04:01 -0600

    practica18

--- parcial3/tkinterSQLite/controladorBD.py
+++ parcial3/tkinterSQLite/controladorBD.py
@@ -1,5 +1,7 @@
 from tkinter import messagebox
 import sqlite3
+import tkinter
+from typing import Self
 import bcrypt
 
 class controladorBD:
@@ -73,3 +75,75 @@ class controladorBD:
 
             except sqlite3.OperationalError:
                 print("Error consulta")
+
+    def impUsuario(self):
+        cons = self.conexionBD()
+
+        try:
+            cursor = cons.cursor()
+            selectQuery = "select * from TBRegistrados"
+
+            cursor.execute(selectQuery)
+            rsUsuarios = cursor.fetchall()
+            cons.close()
+
+            return rsUsuarios
+        
+        except sqlite3.OperationalError:
+            print("Error de Consulta")
+
+        # Método para actualizar un usuario
+    def actualizarUsuario(self, id, nom, cor, con):
+        # 1. Preparar una conexión
+        cons = self.conexionBD()
+
+        # 2. Verificar si el ID está vacío
+        if(id == ""):
+            messagebox.showwarning("Cuidado","ID vacío")
+            cons.close()
+        else:
+            try:
+                # 3. Preparar cursor y query
+                cursor = cons.cursor()
+
+                # 4. Encriptar la contraseña si es necesario
+                if con != "":
+                    con = self.encriptarCon(con)
+
+                # 5. Actualizar los datos del usuario
+                updQuery = f"UPDATE TBRegistrados SET nombre=?, correo=?, contraseña=? WHERE id={id}"
+                datos = (nom, cor, con)
+                cursor.execute(updQuery, datos)
+
+                # 6. Guardar los cambios y cerrar la conexión
+                cons.commit()
+                cons.close()
+
+                messagebox.showinfo("Exito", "Usuario actualizado")
+            except sqlite3.OperationalError:
+                print("Error actualizando usuario")
+
+    def elimUsuario(self, id):
+            # 1. Preparar una conexión
+            cons = self.conexionBD()
+
+            # 2. Verificar si el ID está vacío
+            if(id == ""):
+                messagebox.showwarning("Cuidado","ID vacío")
+                cons.close()
+            else:
+                try:
+                    # 3. Preparar cursor y query
+                    cursor = cons.cursor()
+
+                    # 4. Eliminar el usuario
+                    delQuery = f"DELETE FROM TBRegistrados WHERE id={id}"
+                    cursor.execute(delQuery)
+
+                    # 5. Guardar los cambios y cerrar la conexión
+                    cons.commit()
+                    cons.close()
+
+                    messagebox.showinfo("Exito", "Usuario eliminado")
+                except sqlite3.OperationalError:
+                    print("Error eliminando usuario")
--- parcial3/tkinterSQLite/usuariosCRUD.py
+++ parcial3/tkinterSQLite/usuariosCRUD.py
@@ -1,3 +1,4 @@
+from logging import root
 from tkinter import *
 from tkinter import ttk
 import tkinter as tk
@@ -15,13 +16,45 @@ def ejecutaInsert():
 def ejecutaSelectU():
    usuario = controlador.consultarUsuario(varBus.get())
    for usu in usuario:
+    
     cadena = str(usu[0])+ " " + usu[1]+ " " + usu[2]+ " " + str(usu[3])
 
    if(usuario):
        textEnc.insert("0.0",cadena)
    else:
        messagebox.showinfo("Usuario no encontrado","Seguramente no exita en la base de datos")
-   
+
+def ejecutarConsulta():
+   usuarios = controlador.impUsuario()
+   treeview.delete(*treeview.get_children())
+   if usuarios:
+      for usuario in usuarios:
+         treeview.insert("","end", values = (usuario[0], usuario[1], usuario[2], usuario[3]))
+   else:
+      messagebox.showinfo("No existe usuarios","No encontramos usuarios en la base de datos")
+
+
+def ejecutaUpdate():
+    idUsuario = varIdUp.get()
+    nombreUsuario = varNomUp.get()
+    correoUsuario = varCorUp.get()
+    contrasenaUsuario = varConUp.get()
+    confirmacion = messagebox.askyesno("Confirmación", "¿Estás seguro que deseas actualizar los datos del usuario?")
+    if confirmacion:
+        controlador.actualizarUsuario(idUsuario, nombreUsuario, correoUsuario, contrasenaUsuario)
+        messagebox.showinfo("Actualización exitosa", "Los datos del usuario han sido actualizados en la base de datos.")
+    else:
+        messagebox.showinfo("Actualización cancelada", "Los datos del usuario no han sido actualizados en la base de datos.")
+
+def ejecutaDelete():
+    idUsuario = varIdDel.get()
+    confirmacion = messagebox.askyesno("Confirmación", "¿Estás seguro que deseas eliminar al usuario seleccionado?")
+    if confirmacion:
+        controlador.elimUsuario(idUsuario)
+        messagebox.showinfo("Eliminación exitosa", "El usuario ha sido eliminado de la base de datos.")
+    else:
+        messagebox.showinfo("Eliminación cancelada", "El usuario no ha sido eliminado de la base de datos.")
+
 ventana = Tk()
 ventana.title("CRUD de Usuarios")
 ventana.geometry("500x300")
@@ -33,6 +66,7 @@ pestaña1 = ttk.Frame(panel)
 pestaña2 = ttk.Frame(panel)
 pestaña3 = ttk.Frame(panel)
 pestaña4 = ttk.Frame(panel)
+pestaña5 = ttk.Frame(panel)
 
 # Pestaña1: Formulario de usuarios
 
@@ -48,7 +82,7 @@ Entry(pestaña1, textvariable=varCor).pack()
 
 varCon = StringVar()
 Label(pestaña1, text="Contraseña: ").pack()
-Entry(pestaña1, textvariable=varCon).pack()
+Entry(pestaña1, textvariable=varCon, show="*").pack()
 
 btnGuardar = Button(pestaña1, text="Guardar Usuario", command=ejecutaInsert)
 btnGuardar.pack()
@@ -66,9 +100,64 @@ subBus = Label(pestaña2,text="Encontrado: ", fg='blue', font=("Modern",15)).pac
 textEnc = tk.Text(pestaña2,height=5,width=52)
 textEnc.pack()
 
+#Pestaña 3: Consultar usuarios
+
+titulo3 = Label(pestaña3,text="Consultar Usuarios", fg='green', font=("Modern",18)).pack()
+
+varCons = tk.StringVar()
+botonCons = Button(pestaña3, text = "Buscar", command=ejecutarConsulta).pack()
+
+treeview = ttk.Treeview(pestaña3, columns=(1,2,3,4), show = "headings", height="5")
+treeview.heading(1, text="id")
+treeview.column(1, width=10)
+treeview.heading(2, text="Nombre_Usuario")
+treeview.column(2, width=60)
+treeview.heading(3, text="Correo_Usuario")
+treeview.column(3, width=50)
+treeview.heading(4, text="Contraseña_Uusario")
+treeview.column(4, width=30)
+
+subCons = Label(pestaña3, text="Usuarios que se encontraron: ", fg="black",font=("Modern",18)).pack()
+treeview.pack()
+
+#Pestaña 4: Actualizar Usuario
+
+titulo4 = Label(pestaña4,text="Actualizar y Eliminar", fg='blue', font=("Modern",18)).pack()
+
+varIdUp = StringVar()
+Label(pestaña4, text="ID de Usuario a Actualizar: ").pack()
+Entry(pestaña4, textvariable=varIdUp).pack()
+
+varNomUp = StringVar()
+Label(pestaña4, text="Nombre de Usuario Nuevo: ").pack()
+Entry(pestaña4, textvariable=varNomUp).pack()
+
+varCorUp = StringVar()
+Label(pestaña4, text="Correo de Usuario Nuevo: ").pack()
+Entry(pestaña4, textvariable=varCorUp).pack()
+
+varConUp = StringVar()
+Label(pestaña4, text="Contraseña de Usuario Nueva: ").pack()
+Entry(pestaña4, textvariable=varConUp, show="*").pack()
+
+btnActualizar = Button(pestaña4, text="Actualizar", command=ejecutaUpdate)
+btnActualizar.pack()
+
+#Pestaña 5: Eliminar Usuario
+
+titulo5 = Label(pestaña5,text="Eliminar Usuario", fg='blue', font=("Modern",18)).pack()
+
+varIdDel = StringVar()
+Label(pestaña5, text="ID de Usuario a Eliminar: ").pack()
+Entry(pestaña5, textvariable=varIdDel).pack()
+
+btnEliminar = Button(pestaña5, text="Eliminar Usuario", command=ejecutaDelete)
+btnEliminar.pack()
+
 panel.add(pestaña1, text='Formulario de usuarios')
 panel.add(pestaña2, text='Buscar usuario')
 panel.add(pestaña3, text='Consultar usuarios')
 panel.add(pestaña4, text='Actualizar usuario')
+panel.add(pestaña5, text='Eliminar usuario')
 
 ventana.mainloop()
