fb04735bdf61265d148a5232d02a2dd854bf9eaf
Miguel Quevedo <miguelquevedo53@gmail.com>
2023-04-19 18:54:35 -0600

    3ercommit

--- Examen3/controlador.py
+++ Examen3/controlador.py
@@ -1,10 +1,8 @@
 
 import sqlite3
-from tkinter import messagebox
 import tkinter
 import tkinter as tk
-
-
+from tkinter import messagebox
 
 class controlador:
 
@@ -14,31 +12,111 @@ class controlador:
     # Metodo para crear conexiones
     def conexionBD(self):
 
+
         try:
-            conexion = sqlite3.connect("C:/Users/migue/OneDrive/Documentos/GitHubPOO/ProgramacionS181/Examen3/Ferreteria.db")
+            conexion = sqlite3.connect("Ferreteria.db")
+
             print("Conectado a la Base de Datos")
             return conexion
         except sqlite3.OperationalError:
             print("No se puede conectar")
-            
-    def insertar(self):
-        Material = self.Material_entry.get()
-        Cantidad = self.Cantidad_entry.get()
-        self.cursor.execute("INSERT INTO MatConstruccion VALUES (?, ?, ?)", (Material,Cantidad))
-        self.conexion.commit()
+            from tkinter import messagebox
 
-    def actualizar(self):
-        Material = self.Material_entry.get()
-        Cantidad = self.Cantidad_entry.get()
-        self.cursor.execute("UPDATE MatConstruccion SET Material=?, Cantidad=?", (Material,Cantidad))
-        self.conexion.commit()
+    #Metodo para guardar usuarios
+    def guardarUsuario(self,nom,cor,con):
 
-    def consultar(self):
-        self.resultados_text.delete(1.0, tk.END)
-        Material = self.Material_entry.get()
-        self.cursor.execute("SELECT * FROM MatConstruccion WHERE Material=?", (Material))
-        resultados = self.cursor.fetchall()
-        for resultado in resultados:
-            self.resultados_text.insert(tk.END, f"Material: {resultado[0]}\n")
-            self.resultados_text.insert(tk.END, f"Cantidad: {resultado[1]}\n")
-            
+        # paso 1: usamos una conexion
+        conx = self.conexionBD()
+
+        # paso 2: validar parametros que esten vacios
+        if(nom=="" or cor=="" or con ==""):
+            messagebox.showwarning("Formulario Incompleto")
+        else:
+        # paso 3: preparamos el cursor, datos y QuerySQL
+            cursor = conx.cursor()
+            conH = self.encriptarCon(con)
+            datos = (nom,cor,conH)
+            qrInsert = "insert into MatConstruccion(IDMat, Material, Cantidad) values(?,?,?)"
+
+
+        # paso 4: ejecutar Insert y cerramos conexion
+            cursor.execute(qrInsert,datos)
+            conx.commit()
+            conx.close
+            messagebox.showinfo("Exito","Usuario Guardado")
+
+
+     # Metodo para buscar un usuario
+    def consultarUsuario(self,id):
+        #1. Preparar una conexion
+        cons = self.conexionBD()
+
+        #2. Verifiacar si ID contiene algo
+        if(id == ""):
+            messagebox.showwarning("Cuidado","ID vacío")
+            cons.close()
+        else:
+            try:
+                #3. Preparar cursor y querry
+                cursor = cons.cursor()
+                selecQuery = "select * from MatConstruccion where IDMat="+id
+
+
+                #4. Ejecutar y guardar la consulta
+                cursor.execute(selecQuery)
+                rsUsuario = cursor.fetchall()
+                cons.close()
+
+                return rsUsuario
+
+            except sqlite3.OperationalError:
+                print("Error consulta")
+
+    def impUsuario(self):
+        cons = self.conexionBD()
+
+        try:
+            cursor = cons.cursor()
+            selectQuery = "select * from MatConstruccion"
+
+
+            cursor.execute(selectQuery)
+            rsUsuarios = cursor.fetchall()
+            cons.close()
+
+            return rsUsuarios
+        
+        except sqlite3.OperationalError:
+            print("Error de Consulta")
+
+        # Método para actualizar un usuario
+    def actualizarUsuario(self, id, nom, cor, con):
+        # 1. Preparar una conexión
+        cons = self.conexionBD()
+
+        # 2. Verificar si el ID está vacío
+        if(id == ""):
+            messagebox.showwarning("Cuidado","ID vacío")
+            cons.close()
+        else:
+            try:
+                # 3. Preparar cursor y query
+                cursor = cons.cursor()
+
+                # 4. Encriptar la contraseña si es necesario
+                if con != "":
+                    con = self.encriptarCon(con)
+
+                # 5. Actualizar los datos del usuario
+                updQuery = f"UPDATE MatConstruccion SET Material=?, Cantidad=? WHERE IDMat={id}"
+                datos = (nom, con)
+                cursor.execute(updQuery, datos)
+
+
+                # 6. Guardar los cambios y cerrar la conexión
+                cons.commit()
+                cons.close()
+
+                messagebox.showinfo("Exito", "Usuario actualizado")
+            except sqlite3.OperationalError:
+                print("Error actualizando usuario")
--- Examen3/ventana.py
+++ Examen3/ventana.py
@@ -1,33 +1,134 @@
-from controlador import controlador
+from logging import root
+from tkinter import *
+from tkinter import ttk
 import tkinter as tk
+from tkinter import messagebox
+from controlador import controlador
 
-def __init__(self, master):
-    # Creamos un objeto de la clase controladorBD
-    controlador = controlador()
-    self.master = master
-    self.master.title("Ferreteria")
+# Creamos un objeto de la clase controladorBD
+con = controlador()
 
-    self.Material_label = tk.Label(master, text="Material: ")
-    self.Material_label.grid(row=0, column=0)
-    self.Material_entry = tk.Entry(master)
-    self.Material_entry.grid(row=0, column=1)
+# Función para disparar el botón
+# Función para disparar el botón
+def ejecutaInsert():
+    controlador.guardarUsuario(varNom.get(),varCor.get())
 
-    self.Cantidad_label = tk.Label(master, text="Cantidad:")
-    self.Cantidad_label.grid(row=1, column=0)
-    self.Cantidad_entry = tk.Entry(master)
-    self.Cantidad_entry.grid(row=1, column=1)
+# Función para disparar el boton de busqueda
+def ejecutaSelectU():
+   usuario = controlador.consultarUsuario(varBus.get())
+   for usu in usuario:
+    
+    cadena = str(usu[0])+ " " + usu[1]+ " " + usu[2]+ " " + str(usu[3])
 
-    self.insertar_btn = tk.Button(master, text="Insertar", command=self.insertar)
-    self.insertar_btn.grid(row=3, column=0)
+   if(usuario):
+       textEnc.insert("0.0",cadena)
+   else:
+       messagebox.showinfo("Usuario no encontrado","Seguramente no exita en la base de datos")
 
-    self.actualizar_btn = tk.Button(master, text="Actualizar", command=self.actualizar)
-    self.actualizar_btn.grid(row=3, column=1)
+def ejecutarConsulta():
+   usuarios = controlador.impUsuario()
+   treeview.delete(*treeview.get_children())
+   if usuarios:
+      for usuario in usuarios:
+         treeview.insert("","end", values = (usuario[0], usuario[1], usuario[2], usuario[3]))
+   else:
+      messagebox.showinfo("No existe usuarios","No encontramos usuarios en la base de datos")
 
-    self.consultar_btn = tk.Button(master, text="Consultar", command=self.consultar)
-    self.consultar_btn.grid(row=4, column=0)
+def ejecutaUpdate():
+    idUsuario = varIdUp.get()
+    nombreUsuario = varNomUp.get()
+    correoUsuario = varCorUp.get()
+    confirmacion = messagebox.askyesno("Confirmación", "¿Estás seguro que deseas actualizar los datos del material?")
+    if confirmacion:
+        controlador.actualizarUsuario(idUsuario, nombreUsuario, correoUsuario, contrasenaUsuario)
+        messagebox.showinfo("Actualización exitosa", "Los datos del usuario han sido actualizados en la base de datos.")
+    else:
+        messagebox.showinfo("Actualización cancelada", "Los datos del usuario no han sido actualizados en la base de datos.")
 
-    self.resultados_text = tk.Text(master, height=10, width=30)
-    self.resultados_text.grid(row=5, columnspan=2)
 
-root = tk.Tk()
-root.mainloop()
+ventana = Tk()
+ventana.title("Ferreteria")
+ventana.geometry("500x300")
+
+panel = ttk.Notebook(ventana)
+panel.pack(fill='both', expand='yes')
+
+pestaña1 = ttk.Frame(panel)
+pestaña2 = ttk.Frame(panel)
+pestaña3 = ttk.Frame(panel)
+pestaña4 = ttk.Frame(panel)
+pestaña5 = ttk.Frame(panel)
+
+# Pestaña1: Formulario de Material
+
+Label(pestaña1, text='Registro de Material', fg='blue', font=('Modern', 18)).pack()
+
+varNom = StringVar()
+Label(pestaña1, text="Material: ").pack()
+Entry(pestaña1, textvariable=varNom).pack()
+
+varCor = StringVar()
+Label(pestaña1, text="Cantidad: ").pack()
+Entry(pestaña1, textvariable=varCor).pack()
+
+
+btnGuardar = Button(pestaña1, text="Guardar Usuario", command=ejecutaInsert)
+btnGuardar.pack()
+
+#Pestaña 2: Buscar Material
+
+titulo2 = Label(pestaña2,text="Buscar Material", fg='green', font=("Modern",18)).pack()
+
+varBus = tk.StringVar()
+lblid = Label(pestaña2,text= "Identificador de Materiales: ").pack()
+txtid = Entry(pestaña2,textvariable=varBus).pack()
+btnBus = Button(pestaña2, text="Buscar",command=ejecutaSelectU).pack()
+
+subBus = Label(pestaña2,text="Encontrado: ", fg='blue', font=("Modern",15)).pack()
+textEnc = tk.Text(pestaña2,height=5,width=52)
+textEnc.pack()
+
+#Pestaña 3: Consultar usuarios
+
+titulo3 = Label(pestaña3,text="Consultar Material", fg='green', font=("Modern",18)).pack()
+
+varCons = tk.StringVar()
+botonCons = Button(pestaña3, text = "Buscar", command=ejecutarConsulta).pack()
+
+treeview = ttk.Treeview(pestaña3, columns=(1,2,3,4), show = "headings", height="5")
+treeview.heading(1, text="id")
+treeview.column(1, width=10)
+treeview.heading(2, text="Nombre_Material")
+treeview.column(2, width=60)
+treeview.heading(3, text="Cantidad_Material")
+treeview.column(3, width=50)
+
+subCons = Label(pestaña3, text="Materiales que se encontraron: ", fg="black",font=("Modern",18)).pack()
+treeview.pack()
+
+#Pestaña 4: Actualizar Usuario
+
+titulo4 = Label(pestaña4,text="Actualizar y Eliminar", fg='blue', font=("Modern",18)).pack()
+
+varIdUp = StringVar()
+Label(pestaña4, text="ID de Material a Actualizar: ").pack()
+Entry(pestaña4, textvariable=varIdUp).pack()
+
+varNomUp = StringVar()
+Label(pestaña4, text="Material Nuevo: ").pack()
+Entry(pestaña4, textvariable=varNomUp).pack()
+
+varCorUp = StringVar()
+Label(pestaña4, text="Cantidad de material Nuevo: ").pack()
+Entry(pestaña4, textvariable=varCorUp).pack()
+
+btnActualizar = Button(pestaña4, text="Actualizar", command=ejecutaUpdate)
+btnActualizar.pack()
+
+panel.add(pestaña1, text='Formulario de usuarios')
+panel.add(pestaña2, text='Buscar usuario')
+panel.add(pestaña3, text='Consultar usuarios')
+panel.add(pestaña4, text='Actualizar usuario')
+
+ventana.mainloop()
+
